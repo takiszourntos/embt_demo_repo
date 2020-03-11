8ccac79d00cc730bded3b7c2b24680e654ffb86f
Takis Zourntos <takis.zourntos@emads.org>
2020-03-11 11:42:08 -0400

    Rasp Pi testing crap

---
+++ rasp_pi_1/Debug/src/rasp_pi_1.d
@@ -0,0 +1,1 @@
+src/rasp_pi_1.o: ../src/rasp_pi_1.c
---
+++ rasp_pi_1/src/rasp_pi_1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : rasp_pi_1.c
+ Author      : takis
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
